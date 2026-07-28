7e67c3fa59cbe7039677e1aaf40531623583182a
Vagadiya Smit <smitvagadiya7@gmail.com>
2026-07-28 08:50:53 +0530

    Time: 36 ms (59.78%), Space: 75.9 MB (31.87%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) { // T.C = O(n + 26) = O(n) ,S.C = O(26) = O(1)
+        vector<int> freq(26,0);
+
+        for(char c : s)
+            freq[c - 'a']++;
+
+        string left = "";
+        string mid = "";
+
+        for(int i = 0;i < 26;i++){
+            left.append(freq[i]/2,char('a' + i));
+            if(freq[i] % 2 == 1)
+                mid = char('a' + i);
+
+        }
+        string right = left;
+        reverse(right.begin(),right.end());
+        return left + mid + right;
+    }
+};
